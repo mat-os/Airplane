189077b0991b27d2d9399f2376cda04242931320
mat-os <43048752+mat-os@users.noreply.github.com>
2020-06-02 00:01:09 +0300

    fix

--- Assets/Scripts/Enemies/FightingEnemyState.cs
+++ Assets/Scripts/Enemies/FightingEnemyState.cs
@@ -2,22 +2,26 @@
 
 public class FightingEnemyState : IEnemyState
 {
+    private float speedOfRotation = 0.8f;
+    private float maxDistanceToShoot = 500f;
+    private float minAngelToShoot = 6;
+
     public void Execute(Airplane airplane, GameObject target)
     {
         var targetDir = target.transform.position - airplane.transform.position;
 
         var rotation = Quaternion.LookRotation(targetDir);
 
-        float speedOfRotation = 0.7f;
         airplane.Rb.transform.rotation =
             Quaternion.Slerp(airplane.Rb.transform.rotation, rotation, Time.deltaTime * speedOfRotation);
         
         Vector3 forward = airplane.transform.forward;
         float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
 
-        if (Vector3.Distance(target.transform.position, airplane.transform.position) < 500)
+        var distanceBetween = Vector3.Magnitude(targetDir);
+        if (distanceBetween < maxDistanceToShoot)
         {
-            if (angle > -5.0F && angle < 5.0F)
+            if (angle > -minAngelToShoot && angle < minAngelToShoot)
             {
                 airplane.Shoot();
             }
